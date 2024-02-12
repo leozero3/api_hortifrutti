cf0662f153afb3e25daccd02ef1b70c5bc55b408
Leo Home <realleo2009@hotmail.com>
2024-02-11 22:44:52 -0300

    #45

--- app/Controllers/Http/PedidosController.ts
+++ app/Controllers/Http/PedidosController.ts
@@ -1,3 +1,8 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import CreatePedidoValidator from 'App/Validators/CreatePedidoValidator'
 
-export default class PedidosController {}
+export default class PedidosController {
+  public async store({ request, response, auth }: HttpContextContract) {
+    const payload = await request.validate(CreatePedidoValidator)
+  }
+}
